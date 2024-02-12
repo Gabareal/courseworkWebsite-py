e449f85acd18d9e04d64595aa3b3f3eb11d008a3
Gabareal <119044507+Gabareal@users.noreply.github.com>
2024-02-12 12:42:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,41 @@
 from flask import Flask, render_template, request, url_for, request
-import firebase_admin
-from firebase_admin import db,credentials
 import datetime
+import pyrebase
+config = {
+  "apiKey": "AIzaSyBBKv0bFftovEPZhZ_k2XbjrEoATf68O1A",
+  "authDomain": "coursework-7e5bd.firebaseapp.com",
+  "databaseURL": "https://coursework-7e5bd-default-rtdb.asia-southeast1.firebasedatabase.app",
+  "projectId": "coursework-7e5bd",
+  "storageBucket": "coursework-7e5bd.appspot.com",
+  "messagingSenderId": "627904949119",
+  "appId": "1:627904949119:web:ff6f3d641483b3690ac84a",
+  "measurementId": "G-7B1CH7BFJR",
+  "serviceAccount": "serviceAccount.json",
+  "databaseURL" : "https://coursework-7e5bd-default-rtdb.asia-southeast1.firebasedatabase.app"
+ }
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db = firebase.database()
+#storage = firebase.storage()
 
-cred = credentials.Certificate("api/key.json")
-firebase_admin.initialize_app(cred,{"databaseURL":'https://coursework-7e5bd-default-rtdb.asia-southeast1.firebasedatabase.app'})
-ref = db.reference("/items")
+
 
 app = Flask(__name__)
+@app.route('/',methods=['POST','GET'])
+def login():
+    if request.method == 'POST':
+        user_email = request.form['email']
+        user_password = request.form['password']
+        try:
+            auth.sign_in_with_email_and_password(user_email, user_password)
+            return "Login successful!"
+        except ValueError as e:
+            return "Authentication failed: {}".format(e)
+    else:
+        return render_template('login.html')
 
-@app.route('/', methods=['POST','GET'])
-def index():
+@app.route('/report', methods=['POST','GET'])
+def report():
     if request.method == 'POST':
         form_errors = ""
         if request.form['item_name'] == "":
@@ -22,47 +47,40 @@ def index():
         if request.form['item_owner'] == "":
             form_errors += "Owner "
         if len(form_errors) == 0:
-            itemUpload = ref.child(request.form['item_name'])
-            itemUpload.set({
+            data = {
                 'description': request.form['item_desc'],
                 'date': datetime.datetime.now().strftime("%D"),
                 'location': request.form['item_location'],
                 'owner': request.form['item_owner'],
                 'isFound': False,
-            })
+            }
+            db.child("items").child(request.form['item_name']).set(data)
             return 'Form submitted! Thank you for reporting!'
         else:
             return 'Missing values: {}'.format(form_errors)
     else:
-        return render_template('index.html')
+        return render_template('report.html')
 
 @app.route('/view', methods=['POST','GET'])
 def view():
     if request.method =='POST':
-        data = ref.get()  # Retrieve data from Firebase
+        data = db.child("items").get().val()
+        formatted_data = [{"name": key, **value} for key, value in data.items()]
+        return render_template('view.html', data=formatted_data)
 
-        return render_template('view.html', data=data)
     else:
         return render_template('view.html')
     
 @app.route('/markFound', methods=['POST','GET'])
 def markFound():
     if request.method=='POST':
-        print('hello!')
         item_name = request.form['item_name']
         if item_name =="":
             return 'Missing values: Name'
         else:
-            itemEdit = ref.child(item_name)
-            itemEdit.update({'isFound': True})
+            db.child("items").child(item_name).update({'isFound': True})
             return 'Item edit successful! please check the database!'
-
     else:
         return render_template('markFound.html')
 if __name__ == "__main__":
     app.run(debug=True)
-
-#TO DO:
-#3. Edit database data
-#4. Styling
-#4. Login authorisation OPTIONAL
